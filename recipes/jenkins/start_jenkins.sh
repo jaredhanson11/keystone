2eaedad0f801012593cf44f97e0c190ea0639a55
Jared Hanson <hansonj@mit.edu>
2019-11-16 09:09:44 -0800

    Updated jenkins image

--- recipes/jenkins/start_jenkins.sh
+++ recipes/jenkins/start_jenkins.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 # Ability to set docker.sock location per OS
 DOCKER_SOCK_LOC=${DOCKER_SOCK_LOC:-"/var/run/docker.sock"}
+
+set -x
 docker run -it \
-    -v $DOCKER_SOCK_LOC:/var/run/docker.sock \
-    -e JENKINS_DATA=~/.jenkins-data/ \
-    -d \
-    endergy/jenkins
+-v $DOCKER_SOCK_LOC:/var/run/docker.sock \
+-e JENKINS_DATA=~/.jenkins-data/ \
+-d \
+$@ \
+endergy/jenkins
