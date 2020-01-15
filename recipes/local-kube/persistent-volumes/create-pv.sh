427be55afd50da7240182fa3bbf59d67c657b1ab
Jared Hanson <hansonj@mit.edu>
2020-01-15 00:54:52 -0800

    Some major refactor and cleanup
    Moving toward docker compose for some tooling

--- recipes/local-kube/persistent-volumes/create-pv.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-set -x
-
-# Use this to create PV to store persistent data
-# Usage:
-#   create-pv.sh <pv-name> <local-dir> <allocated-size>
-
-PV_CHART="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )/pv"
-helm template $PV_CHART --set name=$1 --set path=$2 --set size=$3 | kubectl apply -f -
