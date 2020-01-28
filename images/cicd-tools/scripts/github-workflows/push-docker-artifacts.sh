001b733ed3a734145c365e22688d3e0ca50e855e
Jared Hanson <hansonj@mit.edu>
2020-01-27 22:48:17 -0800

    Remove cd $RUNNER_WORKSPACE

--- images/cicd-tools/scripts/github-workflows/push-docker-artifacts.sh
+++ images/cicd-tools/scripts/github-workflows/push-docker-artifacts.sh
@@ -2,6 +2,5 @@
 # This script pushes artifacts in this repository.
 #   pushes are determine by `make push`
 set -x
-cd $RUNNER_WORKSPACE
 docker login -u "${DOCKER_USER}" -p "${DOCKER_PASS}"
 make push
