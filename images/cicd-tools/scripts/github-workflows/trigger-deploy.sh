fb99541d8f6a474c784b884e4c27faf35c169486
Jared Hanson <hansonj@mit.edu>
2020-01-31 22:53:46 -0800

    Fix bug in trigger-deploy

--- images/cicd-tools/scripts/github-workflows/trigger-deploy.sh
+++ images/cicd-tools/scripts/github-workflows/trigger-deploy.sh
@@ -2,5 +2,5 @@
 
 curl --header 'Authorization: token ${GITHUB_KEY}' \
      --header 'Accept: application/vnd.github.ant-man-preview+json' \
-     -X 'POST'
-     --location https://api.github.com/${GITHUB_USER}/${GITHUB_REPO}/deployments
+     -X 'POST' \
+     https://api.github.com/${GITHUB_USER}/${GITHUB_REPO}/deployments
