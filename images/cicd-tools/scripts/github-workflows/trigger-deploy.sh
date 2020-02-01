198a173c005971ee2f0295b8a7134b16fe73fe79
Jared Hanson <hansonj@mit.edu>
2020-01-31 23:39:30 -0800

    Allow docker image job to trigger deployment on itsel

--- images/cicd-tools/scripts/github-workflows/trigger-deploy.sh
+++ images/cicd-tools/scripts/github-workflows/trigger-deploy.sh
@@ -2,6 +2,6 @@
 curl --user "${GITHUB_USER}:${GITHUB_KEY}" \
      --header 'Accept: application/vnd.github.ant-man-preview+json' \
      --header 'Content-Type: application/json' \
-     --data '{"ref":"master"}' \
+     --data '{"ref":"master", "required_contexts": []}' \
      --request 'POST' \
      https://api.github.com/repos/${GITHUB_USER}/${GITHUB_REPO}/deployments
