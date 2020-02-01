2356ab54ef300f5a7d72a733945a9ffcb7b00c57
Jared Hanson <hansonj@mit.edu>
2020-01-31 23:00:50 -0800

    Fixing github access keys

--- images/cicd-tools/scripts/github-workflows/trigger-deploy.sh
+++ images/cicd-tools/scripts/github-workflows/trigger-deploy.sh
@@ -1,6 +1,6 @@
 #! /usr/bin/env bash
 
-curl --header 'Authorization: token ${GITHUB_KEY}' \
+curl --header 'Authorization: token ${GITHUB_USER}:${GITHUB_KEY}' \
      --header 'Accept: application/vnd.github.ant-man-preview+json' \
      -X 'POST' \
      https://api.github.com/${GITHUB_USER}/${GITHUB_REPO}/deployments
