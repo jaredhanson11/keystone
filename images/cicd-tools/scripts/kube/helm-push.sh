8b81c5f761e873f2003769aebbb9f3420fde688d
Jared Hanson <jred0011@gmail.com>
2021-03-07 03:29:28 -0800

    Small helm push change, should be working now

--- images/cicd-tools/scripts/kube/helm-push.sh
+++ images/cicd-tools/scripts/kube/helm-push.sh
@@ -4,7 +4,7 @@
 #   ./helm-push.sh <chart-dir>
 # Requires: $HELM_REPO_USER
 #           $HELM_REPO_PASSWORD
-#           $HELM_REPO_URL
+#           $HELM_REPO_URL (HAS TO END IN /, ex: "https://nexus.goflok.com/repository/helm/")
 set -e
 
 if [[ -z "$HELM_REPO_PASSWORD" || -z "$HELM_REPO_USER" || -z $HELM_REPO_URL ]]; then
@@ -15,7 +15,7 @@ fi
 
 helm dependency update $1 && \
 helm lint $1 --strict --debug && \
-mkdir -p ~/.cicd-tools/helm
-CHART_PACKAGE="$(helm package -d ~/.cicd-tools/helm/ "$1" | cut -d":" -f2 | tr -d '[:space:]')"
-curl -u "$HELM_REPO_USER:$HELM_REPO_PASSWORD" "$HELM_REPO_URL" --upload-file "$CHART_PACKAGE"
+mkdir -p ~/.cicd-tools/helm && \
+CHART_PACKAGE="$(helm package -d ~/.cicd-tools/helm/ "$1" | cut -d":" -f2 | tr -d '[:space:]')" && \
+curl -u "$HELM_REPO_USER:$HELM_REPO_PASSWORD" "$HELM_REPO_URL" --upload-file $CHART_PACKAGE
 
