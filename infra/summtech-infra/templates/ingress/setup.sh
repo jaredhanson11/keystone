743c2f50c5629b0357c008621dabef158cc01ae4
Jared Hanson <jred0011@gmail.com>
2021-09-21 00:52:14 -0700

    Some comments on global accelerator in front of alb

--- infra/summtech-infra/templates/ingress/setup.sh
+++ infra/summtech-infra/templates/ingress/setup.sh
@@ -7,3 +7,11 @@ helm upgrade -i aws-load-balancer-controller eks/aws-load-balancer-controller \
   -n kube-system
 
 k apply -f ingress.yaml
+
+# There still is a step missing where you need to attach a global Accelerator
+# I did this all via the console first time around for ease of use, but it shouldn't be challenging
+# To include pulumi scripts for it.
+
+# The global accelerator gives static IP's for the ALB. We need static IP's for a
+# A record at the root DNS and not a CNAME because CNAME was affecting email deliverability
+# since TXT records weren't being picked up properly when root has CNAME.
